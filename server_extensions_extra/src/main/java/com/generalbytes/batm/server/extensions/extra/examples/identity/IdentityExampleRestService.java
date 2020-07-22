053aebc9ad59cd4635882f66d89f2d2491bc5a6a
zerabat <santiago.taba@gmail.com>
2020-07-22 13:49:07 -0300

    All the extensions are commented except for BTH and identities The register Identity api now allows to add selfie picture and id document scan. It also checks for email, phone number and document number uniqueness

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/examples/identity/IdentityExampleRestService.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/examples/identity/IdentityExampleRestService.java
@@ -23,21 +23,15 @@ import com.generalbytes.batm.server.extensions.IIdentityNote;
 import com.generalbytes.batm.server.extensions.IIdentityPiece;
 import com.generalbytes.batm.server.extensions.ILimit;
 import com.generalbytes.batm.server.extensions.PhoneNumberQueryResult;
+import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.FormParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Base64;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Path("/")
 public class IdentityExampleRestService {
@@ -45,6 +39,7 @@ public class IdentityExampleRestService {
     // Uncomment this example in ***batm-extensions.xml*** and call it for example with:
     // curl -k -XPOST https://localhost:7743/extensions/identity-example/register -d "terminalSerialNumber=BT102239&externalId=EXTID0001&fiatCurrency=USD&limit=1000000&discount=100&phoneNumber=+12065550100&firstName=Chuck&lastName=Norris&emailAddress=chucknorrisfans@hotmail.com&idCardNumber=123456&contactZIP=77868&contactCountry=United States&contactProvince=TX&contactCity=Navasota&contactAddress=4360 Lone Wolf Ranch Road"
 
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(IdentityExampleRestService.class);
     @POST
     @Path("/register")
     @Produces(MediaType.APPLICATION_JSON)
@@ -56,25 +51,75 @@ public class IdentityExampleRestService {
                            @FormParam("idCardNumber") String idCardNumber, @FormParam("documentValidToYYYYMMDD") String documentValidToYYYYMMDD,
                            @FormParam("contactZIP") String contactZIP,
                            @FormParam("contactCountry") String contactCountry, @FormParam("contactProvince") String contactProvince,
-                           @FormParam("contactCity") String contactCity, @FormParam("contactAddress") String contactAddress) throws ParseException {
+                           @FormParam("contactCity") String contactCity, @FormParam("contactAddress") String contactAddress,
+                           @FormParam("selfieImage") String selfieImage, @FormParam("idScan") String idScan) throws ParseException {
+
+        log.info("fiatCurrency   "+ fiatCurrency );
+        log.info("externalId   " + externalId);
+        log.info("limit   " + limit);
+        log.info("discount    " + discount);
+        log.info("terminalSerialNumber    " + terminalSerialNumber);
+        log.info("note    " + note);
+        log.info("phoneNumber     " + phoneNumber);
+        log.info("firstName   " + firstName);
+        log.info("lastName    " + lastName);
+        log.info("emailAddress    " + emailAddress);
+        log.info("idCardNumber    " + idCardNumber);
+        log.info("documentValidToYYYYMMDD     "+ documentValidToYYYYMMDD);
+        log.info("contactZIP      " + contactZIP);
+        log.info("contactCountry      " + contactCountry);
+        log.info("contactProvince     " + contactProvince);
+        log.info("contactCity     " + contactCity);
+        log.info("contactAddress     " + contactAddress);
 
 
         IExtensionContext ctx = IdentityExampleExtension.getExtensionContext();
-        List<ILimit> limits = Arrays.asList(new LimitExample(fiatCurrency, limit));
-
+        List<ILimit> limits = Collections.singletonList(new LimitExample(fiatCurrency, limit));
+        if (phoneNumber.startsWith(" ")){
+            phoneNumber = phoneNumber.replaceFirst(" ", "+");
+        }
         PhoneNumberQueryResult phoneNumberQueryResult = ctx.queryPhoneNumber(phoneNumber, terminalSerialNumber);
+
         if (phoneNumberQueryResult.isQuerySuccessful()) {
             if (phoneNumberQueryResult.isLineTypeBlocked()) {
                 return "PHONE BLOCKED";
             }
-            System.out.println("Phone type: " + phoneNumberQueryResult.getPhoneLineType().getPhoneTypeCode().name());
         }
 
         int state = IIdentity.STATE_NOT_REGISTERED;
         Date now = new Date();
 
-        // read the image data from the request or a file
-        byte[] exampleJpeg = Base64.getDecoder().decode("/9j/2wBDACQZGyAbFyQgHiApJyQrNls7NjIyNm9PVEJbhHSKiIF0f32Ro9GxkZrFnX1/tve4xdje6uzqja////7j/9Hl6uH/2wBDAScpKTYwNms7O2vhln+W4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eH/wgARCAAFAAUDAREAAhEBAxEB/8QAFAABAAAAAAAAAAAAAAAAAAAAAf/EABQBAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhADEAAAAQ//xAAVEAEBAAAAAAAAAAAAAAAAAAACAf/aAAgBAQABBQImq//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQMBAT8Bf//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQIBAT8Bf//EABgQAAMBAQAAAAAAAAAAAAAAAAECEQCh/9oACAEBAAY/AmAaTu//xAAWEAEBAQAAAAAAAAAAAAAAAAABABH/2gAIAQEAAT8hYg3ik//aAAwDAQACAAMAAAAQH//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQMBAT8Qf//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQIBAT8Qf//EABkQAAIDAQAAAAAAAAAAAAAAAAERMUFRkf/aAAgBAQABPxC1HhihlLD3WT//2Q==");
+        List<IIdentity> a = ctx.findIdentitiesByDocumentNumber(idCardNumber);
+                if (a!= null && !a.isEmpty()){
+            return "Error: idCardNumber is already in use ";
+        }
+
+        a = ctx.findIdentitiesByPhoneNumber(phoneNumber);
+        if (a!= null && !a.isEmpty()){
+            return "Error: phoneNumber already in use ";
+        }
+
+//        STATE_NOT_REGISTERED = 0;
+//        STATE_REGISTERED = 1;
+//        STATE_TO_BE_REGISTERED = 2;
+//        STATE_PROHIBITED = 3;
+//        STATE_ANONYMOUS = 4;
+//        STATE_PROHIBITED_TO_BE_REGISTERED   = 5;
+        List<IIdentity> identities = new ArrayList<>();
+        for (int i=0; i<=5; i++){
+            identities.addAll(ctx.findAllIdentitiesByState(i));
+        }
+        List <IIdentityPiece> identityPieces = identities.stream().flatMap(x -> x.getIdentityPieces().stream()).collect(Collectors.toList());
+
+//        Optional<IIdentityPiece> iIdentityPiece = identityPieces.stream().filter(x -> x.getPhoneNumber().equals(phoneNumber) ||
+//            x.getEmailAddress().equalsIgnoreCase(emailAddress) || x.getIdCardNumber().equals(idCardNumber)).findFirst();
+//        log.info("identities size: " + String.valueOf(identities.size()));
+//        log.info("identityPieces size: " + String.valueOf(identityPieces.size()));
+
+        Optional<IIdentityPiece> iIdentityPiece = identityPieces.stream().filter(x -> x.getEmailAddress()!=null && x.getEmailAddress().equalsIgnoreCase(emailAddress)).findFirst();
+        if (iIdentityPiece.isPresent()){
+            return "Error: email address already in use";
+        }
 
         IIdentity identity = ctx.addIdentity(fiatCurrency, terminalSerialNumber, externalId, limits, limits, limits, limits, limits, note, state, discount, discount, now, now);
         String identityPublicId = identity.getPublicId();
@@ -83,9 +128,19 @@ public class IdentityExampleRestService {
             contactZIP, contactCountry, contactProvince, contactCity, contactAddress));
         ctx.addIdentityPiece(identityPublicId, IdentityPieceExample.fromPhoneNumber(phoneNumber));
         ctx.addIdentityPiece(identityPublicId, IdentityPieceExample.fromEmailAddress(emailAddress));
-        ctx.addIdentityPiece(identityPublicId, IdentityPieceExample.fromSelfie("image/jpeg", exampleJpeg));
-        ctx.addIdentityPiece(identityPublicId, IdentityPieceExample.fromIdScan("image/jpeg", exampleJpeg));
 
+        if (selfieImage != null && !selfieImage.isEmpty()){
+            //ws may replace "+" with " "
+            selfieImage = selfieImage.replaceAll(" " , "+");
+            byte[] selfie = Base64.getMimeDecoder().decode(selfieImage);
+            ctx.addIdentityPiece(identityPublicId, IdentityPieceExample.fromSelfie("image/jpeg", selfie));
+        }
+        if (idScan != null && !idScan.isEmpty()){
+            //ws may replace "+" with " "
+            idScan = idScan.replaceAll(" " , "+");
+            byte[] id = Base64.getMimeDecoder().decode(idScan);
+            ctx.addIdentityPiece(identityPublicId, IdentityPieceExample.fromIdScan("image/jpeg", id));
+        }
         return identityPublicId;
     }
 
@@ -126,5 +181,4 @@ public class IdentityExampleRestService {
 
         return identity.getNotes();
     }
-
 }
